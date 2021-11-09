e10d1e1b5e99b1dc076d7b9f223c1458def4cc49
Albert Li <albert.li@salesforce.com>
2021-11-09 09:54:31 -0800

    fix: node refresh

--- tests/dummy/app/controllers/noderefresh.js
+++ tests/dummy/app/controllers/noderefresh.js
@@ -20,30 +20,4 @@ export default class NoderefreshController extends Controller {
       title: this.randomWord(),
     });
   }
-
-  @action
-  getChildren(node) {
-    return later(
-      this,
-      function () {
-        let i, o, _results;
-
-        o = Math.floor(Math.random() * this.words.length) + 1;
-        if (node.get('level') < 4) {
-          i = 0;
-          _results = [];
-          while (i < o) {
-            node.createChild({
-              title: this.randomWord(),
-            });
-            _results.push(i++);
-          }
-          return _results;
-        } else {
-          return node.emptyChildren();
-        }
-      },
-      500
-    );
-  }
 }
